f8c0fb2fc9e1b9025b655952a4fd1f6449970302
vistor <vistormu@gmail.com>
2023-10-30 10:11:20 -0400

    latest

--- tests/test_agp_regressor.py
+++ tests/test_agp_regressor.py
@@ -10,38 +10,62 @@ from catasta.entities import EvalInfo
 def main() -> None:
     n_inducing_points: int = 128
     n_dim: int = 20
-    dataset = ModelDataset(root="tests/data/nylon_strain/", n_dim=n_dim)
-    # dataset = ModelDataset(root="tests/data/nylon_stress/", n_dim=n_dim)
+    # dataset = ModelDataset(root="tests/data/nylon_strain/", n_dim=n_dim)
+    dataset = ModelDataset(root="tests/data/nylon_stress/", n_dim=n_dim)
     model = ApproximateGPRegressor(n_inducing_points, n_dim, kernel="rq", mean="zero")
+    model.load("tests/models/model.pt")
     scaffold = GaussianRegressorScaffold(model, dataset)
+    scaffold.train_split = 6/7
 
-    losses: np.ndarray = scaffold.train(
-        epochs=100,
-        batch_size=256,
-        train_split=6/7,
-        lr=1e-3,
-    )
+    # losses: np.ndarray = scaffold.train(
+    #     epochs=100,
+    #     batch_size=256,
+    #     train_split=6/7,
+    #     lr=1e-3,
+    # )
 
-    plt.figure(figsize=(30, 20))
-    plt.plot(losses)
-    plt.show()
+    # model.save("tests/models/model.pt")
+
+    # plt.figure(figsize=(30, 20))
+    # plt.plot(losses)
+    # plt.show()
 
     info: EvalInfo = scaffold.evaluate()
 
-    plt.figure(figsize=(30, 20))
-    plt.plot(info.predicted, label="true")
-    plt.plot(info.real, label="pred")
-    if info.stds is not None:
-        plt.fill_between(range(len(info.predicted)),
-                         info.predicted - info.stds,
-                         info.predicted + info.stds,
-                         alpha=0.5,
+    input = info.input[:, -1].flatten()[1500:1725]
+    real = info.real[1500:1725]
+    predicted = info.predicted[1500:1725]
+    stds = info.stds[1500:1725] if info.stds is not None else None
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(input, real, label="true", color="#2f2f2f")
+    plt.plot(input, predicted, label="predicted", color="#0B7AD5")
+    if stds is not None:
+        plt.fill_between(input,
+                         predicted - stds,
+                         predicted + stds,
+                         color="#90caf9",
+                         alpha=0.75,
                          label="std",
                          )
     plt.legend()
     plt.show()
 
-    print(info)
+    # plt.figure(figsize=(10, 10))
+    # plt.plot(real, label="true", color="#2f2f2f")
+    # plt.plot(predicted, label="predicted", color="#0B7AD5")
+    # if stds is not None:
+    #     plt.fill_between(range(len(predicted)),
+    #                      predicted - stds,
+    #                      predicted + stds,
+    #                      color="#90caf9",
+    #                      alpha=0.75,
+    #                      label="std",
+    #                      )
+    # plt.legend()
+    # plt.show()
+
+    # print(info)
 
 
 if __name__ == '__main__':
--- tests/test_feedforward_regressor.py
+++ tests/test_feedforward_regressor.py
@@ -10,6 +10,7 @@ from catasta.entities import EvalInfo
 def main() -> None:
     n_dim: int = 20
     dataset: ModelDataset = ModelDataset(root="tests/data/nylon_strain/", n_dim=n_dim)
+    # dataset: ModelDataset = ModelDataset(root="tests/data/nylon_stress/", n_dim=n_dim)
 
     model: FeedforwardRegressor = FeedforwardRegressor(input_dim=n_dim,
                                                        hidden_dims=[64, 32, 16],
@@ -25,11 +26,44 @@ def main() -> None:
                                         lr=1e-3,
                                         )
 
-    plt.figure(figsize=(30, 20))
-    plt.plot(losses)
-    plt.show()
+    # plt.figure(figsize=(30, 20))
+    # plt.plot(losses)
+    # plt.show()
 
     info: EvalInfo = scaffold.evaluate()
+
+    # input = info.input[:, -1].flatten()[1500:1725]
+    # real = info.real[1500:1725]
+    # predicted = info.predicted[1500:1725]
+    # stds = info.stds[1500:1725] if info.stds is not None else None
+
+    # plt.figure(figsize=(10, 10))
+    # plt.plot(input, real, label="true", color="#2f2f2f")
+    # plt.plot(input, predicted, label="predicted", color="#0B7AD5")
+    # if stds is not None:
+    #     plt.fill_between(predicted,
+    #                      predicted - stds,
+    #                      predicted + stds,
+    #                      color="#90caf9",
+    #                      alpha=0.75,
+    #                      label="std",
+    #                      )
+    # plt.legend()
+    # plt.savefig("tests/figures/stress_hysteresis_ff.svg")
+
+    # plt.figure(figsize=(10, 10))
+    # plt.plot(real, label="true", color="#2f2f2f")
+    # plt.plot(predicted, label="predicted", color="#0B7AD5")
+    # if stds is not None:
+    #     plt.fill_between(range(len(predicted)),
+    #                      predicted - stds,
+    #                      predicted + stds,
+    #                      color="#90caf9",
+    #                      alpha=0.75,
+    #                      label="std",
+    #                      )
+    # plt.legend()
+    # plt.savefig("tests/figures/stress_plot_ff.svg")
 
     print(info)
 
