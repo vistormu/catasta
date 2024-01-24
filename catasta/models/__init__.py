60e51979efcc714d3990474208f62b3a3bc163c8
vistor <vistormu@gmail.com>
2024-01-24 11:46:55 +0100

    0.0.1

--- catasta/models/__init__.py
+++ catasta/models/__init__.py
@@ -2,4 +2,3 @@ from .regressors.approximate_gp_regressor import ApproximateGPRegressor
 from .regressors.feedforward_regressor import FeedforwardRegressor
 from .regressors.transformer_regressor.transformer_regressor import TransformerRegressor
 from .regressors.rbf_regressor import RBFRegressor
-from .regressors.mamba_regressor import MambaRegressor
