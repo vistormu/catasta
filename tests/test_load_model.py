b0273af346e445432f10c6d29d77d9457db98847
vistor <vistormu@gmail.com>
2024-01-31 10:13:32 +0100

    tests

--- tests/test_load_model.py
+++ tests/test_load_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from catasta.models import TransformerRegressor, ApproximateGPRegressor
+from catasta.models import TransformerRegressor, ApproximateGPRegressor, FeedforwardRegressor
 from catasta.archways import RegressionArchway
 from catasta.transformations import (
     WindowSliding,
@@ -16,16 +16,21 @@ def main() -> None:
     path: str = "tests/models/"
 
     n_dim: int = 16
-    model = TransformerRegressor(
-        context_length=n_dim,
-        n_patches=2,
-        d_model=64,
-        n_heads=2,
-        n_layers=4,
-        feedforward_dim=32,
-        head_dim=2,
+    # model = TransformerRegressor(
+    #     context_length=n_dim,
+    #     n_patches=2,
+    #     d_model=64,
+    #     n_heads=2,
+    #     n_layers=4,
+    #     feedforward_dim=32,
+    #     head_dim=2,
+    #     dropout=0.0,
+    #     use_fft=True,
+    # )
+    model = FeedforwardRegressor(
+        input_dim=n_dim,
+        hidden_dims=[8, 16, 8],
         dropout=0.0,
-        use_fft=True,
     )
     # model = ApproximateGPRegressor(
     #     n_inducing_points=16,
