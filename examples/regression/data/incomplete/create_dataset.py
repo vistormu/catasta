ebce40dcd59a91b4dda630334a2252a061a20b25
vistor <vistormu@gmail.com>
2024-04-17 19:19:48 +0200

    adding examples

--- examples/regression/data/incomplete/create_dataset.py
+++ examples/regression/data/incomplete/create_dataset.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 
@@ -17,6 +19,24 @@ def f(x: np.ndarray, noise: np.ndarray) -> np.ndarray:
 output = f(input, np.random.normal(noise_mean, noise_std, n_samples))
 true_output = f(true_input, np.zeros_like(true_input))
 
+# divide into train and val
+n_train = 800
+n_val = n_samples - n_train
+
+# take random samples
+train_indices = np.random.choice(n_samples, n_train, replace=False)
+val_indices = np.setdiff1d(np.arange(n_samples), train_indices)
+
+train_true_input = true_input[train_indices]
+train_true_output = true_output[train_indices]
+train_input = input[train_indices]
+train_output = output[train_indices]
+
+val_true_input = true_input[val_indices]
+val_true_output = true_output[val_indices]
+val_input = input[val_indices]
+val_output = output[val_indices]
+
 data = {
     "input": input,
     "output": output,
@@ -25,3 +45,27 @@ data = {
 }
 
 pd.DataFrame(data).to_csv("data.csv", index=False)
+
+train_data = {
+    "input": train_input,
+    "output": train_output,
+    "true_input": train_true_input,
+    "true_output": train_true_output,
+}
+
+if not os.path.exists("train"):
+    os.makedirs("train")
+
+pd.DataFrame(train_data).to_csv("train/data.csv", index=False)
+
+val_data = {
+    "input": val_input,
+    "output": val_output,
+    "true_input": val_true_input,
+    "true_output": val_true_output,
+}
+
+if not os.path.exists("val"):
+    os.makedirs("val")
+
+pd.DataFrame(val_data).to_csv("val/data.csv", index=False)
--- examples/regression/transformer.py
+++ examples/regression/transformer.py
@@ -1,41 +1,34 @@
-import os
-
+from catasta import Scaffold, CatastaDataset
 from catasta.models import TransformerRegressor
-from catasta.datasets import RegressionDataset
-from catasta.scaffolds import Scaffold
-from catasta.dataclasses import RegressionEvalInfo, TrainInfo
 from catasta.transformations import (
     Normalization,
-    Decimation,
     WindowSliding,
     Slicing,
     Custom,
 )
+
+from vclog import Logger
 
 
 def main() -> None:
     n_dim: int = 768
     dataset_root: str = "data/nylon_wire/"
     input_trasnsformations = [
-        Custom(lambda x: x[500_000:1_000_000]),
+        Custom(lambda x: x[:10_000]),
         Normalization("minmax"),
-        Decimation(decimation_factor=10),
         WindowSliding(window_size=n_dim, stride=1),
     ]
     output_trasnsformations = [
-        Custom(lambda x: x[500_000:1_000_000]),
+        Custom(lambda x: x[:10_000]),
         Normalization("minmax"),
-        Decimation(decimation_factor=10),
         Slicing(amount=n_dim - 1, end="left"),
     ]
 
-    n_files = len(os.listdir(dataset_root))
-    train_split = (n_files-1) / n_files
-    dataset = RegressionDataset(
+    dataset = CatastaDataset(
         root=dataset_root,
+        task="regression",
         input_transformations=input_trasnsformations,
         output_transformations=output_trasnsformations,
-        splits=(train_split, 1-train_split, 0.0)
     )
 
     model = TransformerRegressor(
@@ -55,15 +48,16 @@ def main() -> None:
         loss_function="mse",
     )
 
-    train_info: TrainInfo = scaffold.train(
-        epochs=10,
+    scaffold.train(
+        epochs=100,
         batch_size=256,
         lr=1e-3,
+        final_lr=1e-4,
+        early_stopping=(10, 0.01),
     )
-    print(f"min eval loss: {train_info.best_val_loss:.4f}")
-
     info = scaffold.evaluate()
-    print(info)
+
+    Logger.debug(info)
 
 
 if __name__ == '__main__':
