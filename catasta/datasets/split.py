ea971e8f038b29fc000cc5618c691bb8cc3438f2
vistor <vistormu@gmail.com>
2024-05-20 12:06:14 +0200

    added split support for regression

--- catasta/datasets/split.py
+++ catasta/datasets/split.py
@@ -2,28 +2,100 @@ import os
 import random
 import shutil
 
+import pandas as pd
+
 
 EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp", ".csv")
 
 
-def split_dataset(dataset: str, destination: str, task: str, splits: tuple[float, float, float], shuffle: bool) -> None:
+def split_dataset(dataset: str,
+                  destination: str,
+                  task: str,
+                  splits: tuple[float, float, float],
+                  shuffle: bool,
+                  ) -> None:
+    """ Split a dataset into training, validation, and testing sets.
+
+    Parameters
+    ----------
+    dataset : str
+        The path to the dataset.
+    destination : str
+        The path to save the split dataset.
+    task : str
+        The task type, either "classification" or "regression".
+    splits : tuple[float, float, float]
+        The split ratios for training, validation, and testing sets.
+    shuffle : bool
+        Whether to shuffle the dataset before splitting.
+
+    Raises
+    ------
+    ValueError
+        If the splits do not sum to 1.0, if the training split is 0, or if both the validation and testing splits are 0.
+        If the task is not supported.
+        If the task is classification and the dataset is not a directory.
+        If the task is regression and the dataset is not a file.
+    """
     train_split: float = splits[0]
     val_split: float = splits[1]
     test_split: float = splits[2]
 
+    _check_arguments(dataset, task, train_split, val_split, test_split)
+
+    os.makedirs(os.path.join(destination, "training"), exist_ok=True)
+    os.makedirs(os.path.join(destination, "validation"), exist_ok=True) if val_split else None
+    os.makedirs(os.path.join(destination, "testing"), exist_ok=True) if test_split else None
+
+    split_function = _classification_split if task == "classification" else _regression_split
+    split_function(dataset, destination, shuffle, train_split, val_split, test_split)
+
+
+def _check_arguments(dataset: str,
+                     task: str,
+                     train_split: float,
+                     val_split: float,
+                     test_split: float,
+                     ) -> None:
     if abs(train_split + val_split + test_split - 1.0) > 1e-6:
         raise ValueError("Splits must sum to 1.0")
     if not train_split:
         raise ValueError("Train split must be greater than 0")
     if not val_split and not test_split:
         raise ValueError("At least one of val split or test split must be greater than 0")
-
-    if task == "classification":
-        _classification_split(dataset, destination, shuffle, train_split, val_split, test_split)
-    elif task == "regression":
-        raise NotImplementedError("Regression task not implemented")
-    else:
+    if task not in ("classification", "regression"):
         raise ValueError(f"Task {task} not supported")
+    if task == "classification" and not os.path.isdir(dataset):
+        raise ValueError("Dataset must be a directory")
+    if task == "regression" and not os.path.isfile(dataset):
+        raise ValueError("Dataset must be a file")
+
+
+def _regression_split(dataset: str,
+                      destination: str,
+                      shuffle: bool,
+                      train_split: float,
+                      val_split: float,
+                      test_split: float,
+                      ) -> None:
+    df = pd.read_csv(dataset)
+    n_samples = len(df)
+    indices = list(range(n_samples))
+
+    if shuffle:
+        random.shuffle(indices)
+
+    train_index = int(train_split * n_samples)
+    val_index = train_index + int(val_split * n_samples)
+    test_index = val_index + int(test_split * n_samples)
+
+    train = df.iloc[indices[:train_index]]
+    val = df.iloc[indices[train_index:val_index]] if val_split else pd.DataFrame()
+    test = df.iloc[indices[val_index:test_index]] if test_split else pd.DataFrame()
+
+    train.to_csv(os.path.join(destination, "training.csv"), index=False)
+    val.to_csv(os.path.join(destination, "validation.csv"), index=False) if val_split else None
+    test.to_csv(os.path.join(destination, "testing.csv"), index=False) if test_split else None
 
 
 def _classification_split(dataset: str,
@@ -54,19 +126,16 @@ def _classification_split(dataset: str,
         test = images[val_index:test_index] if test_split else []
 
         train_path = os.path.join(destination, "training", cls)
-        os.makedirs(train_path, exist_ok=True)
         for img in train:
             train_img = os.path.join(train_path, os.path.basename(img))
             shutil.copyfile(img, train_img)
 
         val_path = os.path.join(destination, "validation", cls)
-        os.makedirs(val_path, exist_ok=True) if val else None
         for img in val:
             val_img = os.path.join(val_path, os.path.basename(img))
             shutil.copyfile(img, val_img)
 
         test_path = os.path.join(destination, "testing", cls)
-        os.makedirs(test_path, exist_ok=True) if test else None
         for img in test:
             test_img = os.path.join(test_path, os.path.basename(img))
             shutil.copyfile(img, test_img)
